b1cebcae1ba7591034172b12bd8bedf6f9eb7251
proyectos-developer <proyectos@developer-ideas.com>
2024-01-11 12:33:16 -0500

    V1.0.7

--- client/src/uri/constantes.js
+++ client/src/uri/constantes.js
@@ -1,7 +1,7 @@
 export const constantes = () => {
     return {
         url_principal: [
-            {url: 'http://localhost:3001/api'}
+            {url: 'https://admin-comfisa.developer-ideas.com/api'}
         ],
     }
 }
