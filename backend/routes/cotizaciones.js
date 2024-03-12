20158dedfd6e8fb8a4ba446ac825efca3418b958
proyectos-developer <proyectos@developer-ideas.com>
2024-03-12 14:11:15 -0500

    V1.0.22

--- backend/routes/cotizaciones.js
+++ backend/routes/cotizaciones.js
@@ -143,7 +143,7 @@ router.get ('/api/cotizaciones/usuario/:shop_id', async (req, res) => {
         const total = await pool.query ('SELECT * FROM carrito_cotizacion WHERE shop_id = ?', [shop_id])
         const usuario = cotizaciones[0].usuario
         console.log ('usuario', usuario)
-        const cliente = await pool.query ('SELECT * FROM clientes WHERE usuario = ?', [usuario])
+        const cliente = await pool.query ('SELECT * FROM info_clientes WHERE usuario = ?', [usuario])
         
         return res.json ({
             cotizaciones: cotizaciones[0],
